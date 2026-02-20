602beb01121c4dec36a0fbf3c3b830a4e5d5579d
Sovit8917 <lenkasovitsarbeswar@gmail.com>
2026-02-20 16:55:01 +0530

    redeploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://personal-finance-tracker-eo0x.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
