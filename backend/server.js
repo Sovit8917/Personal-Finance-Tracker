17af084f94d3e681682934177c3e97d47c53ae2a
Sovit8917 <lenkasovitsarbeswar@gmail.com>
2026-02-20 16:48:53 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const transactionRoutes = require('./routes/transactions');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://personal-finance-tracker-sovit.netlify.app/', credentials: true }));
 app.use(express.json());
 
 // Routes
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://personal-finance-tracker-eo0x.onrender.com/',
         changeOrigin: true,
       },
     },
